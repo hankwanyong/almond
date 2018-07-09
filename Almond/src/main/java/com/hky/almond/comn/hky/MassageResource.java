32f4dde1a5d9fa5eb694d3c9d19a97f6909e83cc
seeroo <seeroo@DESKTOP-I47NBJ4>
2018-07-09 16:45:23 +0900

    git add source

---
+++ Almond/src/main/java/com/hky/almond/comn/hky/MassageResource.java
@@ -0,0 +1,6 @@
+package com.hky.almond.comn.hky;
+
+public class MassageResource {
+	
+
+}
---
+++ Almond/src/main/java/com/hky/almond/test/massagePropertiesTest.java
@@ -0,0 +1,11 @@
+package com.hky.almond.test;
+
+public class massagePropertiesTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
